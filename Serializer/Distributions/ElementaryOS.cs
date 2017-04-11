5c1b15b0d0cba717763847d2b01948829fcbb792
Yuliya Novik <Yuliya Novik>
2017-04-12 02:31:45 +0300

    Create ElementaryOS class

---
+++ Serializer/Distributions/ElementaryOS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializer.Distributions {
+    public class ElementaryOS : Ubuntu {
+        public ElementaryOS() {
+        }
+
+        public ElementaryOS(string version) : base (version) {
+        }
+    }
+}
