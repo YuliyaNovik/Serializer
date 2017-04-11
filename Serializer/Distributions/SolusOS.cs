a3291e0478f44e86919375bb7eea305c7f9ed62e
Yuliya Novik <Yuliya Novik>
2017-04-12 02:26:57 +0300

    Create SolusOS class

---
+++ Serializer/Distributions/SolusOS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializer.Distributions {
+    public class SolusOS : LinuxMintDebianEdition{
+        public SolusOS() {
+        }
+
+        public SolusOS(string version) : base (version) {
+        }
+    }
+}
