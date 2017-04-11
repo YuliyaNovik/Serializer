e639ba3c182d8359756c920980500ddb784cdd9f
Yuliya Novik <Yuliya Novik>
2017-04-12 02:21:34 +0300

    Create LinuxMintDebianEdition class

---
+++ Serializer/Distributions/LinuxMintDebianEdition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializer.Distributions {
+    public class LinuxMintDebianEdition : Debian{
+        public LinuxMintDebianEdition() { }
+
+        public LinuxMintDebianEdition(string version) : base (version) {
+        }
+        
+    }
+}
