d25c7d47d8d6f1450e9fc64cc134cacebb5de800
Yuliya Novik <Yuliya Novik>
2017-04-12 02:12:44 +0300

    Create Ubuntu class

---
+++ Serializer/Distributions/Ubuntu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializer.Distributions {
+    class Ubuntu : Debian {
+
+        public Ubuntu() { }
+
+        public Ubuntu(string version) : base(version) {
+        }
+
+    }
+}
