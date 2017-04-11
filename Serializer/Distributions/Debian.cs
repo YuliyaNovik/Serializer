22c4c78092b999bf4f91b3cc7cab66cd506f944f
Yuliya Novik <Yuliya Novik>
2017-04-12 02:08:54 +0300

    Create Debian class

---
+++ Serializer/Distributions/Debian.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializer.Distributions {
+    class Debian {
+
+        public string version { get; set; }
+
+        public Debian (){}
+
+        public Debian (string version) {
+            this.version = version;
+        }
+
+    }
+}
