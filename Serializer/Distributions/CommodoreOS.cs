2f28d6e06ecab80e9c6b9b3271fcdd204315d806
Yuliya Novik <Yuliya Novik>
2017-04-12 02:35:07 +0300

    Create CommodoreOS class

---
+++ Serializer/Distributions/CommodoreOS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializer.Distributions {
+    public class CommodoreOS : LinuxMint {
+        public CommodoreOS() {
+        }
+
+        public CommodoreOS(string version) : base(version) {
+        }
+    }
+}
