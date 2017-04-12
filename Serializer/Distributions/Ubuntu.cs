d00e33a364bc1f9f82e983d0513f00030c42c2ab
Yuliya Novik <Yuliya Novik>
2017-04-12 03:21:27 +0300

    Add [XMLRoot] attribute

--- Serializer/Distributions/Ubuntu.cs
+++ Serializer/Distributions/Ubuntu.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Serializer.Distributions {
-    class Ubuntu : Debian {
+    [XmlRoot]
+    public class Ubuntu : Debian {
 
         public Ubuntu() { }
 
