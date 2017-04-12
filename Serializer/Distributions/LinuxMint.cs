0d05e735a98a202c5d5a9ec3470d80bbc86a2867
Yuliya Novik <Yuliya Novik>
2017-04-12 03:21:44 +0300

    Add [XMLRoot] attribute

---
+++ Serializer/Distributions/LinuxMint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serializer.Distributions {
+    [XmlRoot]
+    public class LinuxMint : Ubuntu {
+        public LinuxMint() {
+        }
+
+        public LinuxMint(string version) : base (version) {
+        }
+    }
+}
