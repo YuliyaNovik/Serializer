c25ff40c22d649eea8ce7d58d8ee22f77ce917d8
Yuliya Novik <Yuliya Novik>
2017-04-12 03:21:53 +0300

    Add [XMLRoot] attribute

--- Serializer/Distributions/LinuxMintDebianEdition.cs
+++ Serializer/Distributions/LinuxMintDebianEdition.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Serializer.Distributions {
+    [XmlRoot]
     public class LinuxMintDebianEdition : Debian{
         public LinuxMintDebianEdition() { }
 
